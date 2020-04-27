7d7d94d74db91059983157450eb0b3b88d143174
Luis Correa <correaleyval@gmail.com>
2020-04-27 19:45:18 -0400

    Food model filter

--- api/modules/v1/models/Food.php
+++ api/modules/v1/models/Food.php
@@ -4,4 +4,22 @@ namespace api\modules\v1\models;
 
 class Food extends \common\models\Post
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+
+        array_push($rules, ['category', 'default', 'value' => 'food']);
+
+        return $rules;
+    }
+
+    public static function find()
+    {
+        $query = parent::find();
+
+        return $query->andWhere(['category' => 'food']);
+    }
 }
