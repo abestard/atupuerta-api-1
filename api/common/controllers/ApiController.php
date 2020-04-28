6f42a847753a0979218f74e4f3d812ad9384778e
Luis Correa <correaleyval@gmail.com>
2020-04-27 21:30:05 -0400

    Enable swagger

--- api/common/controllers/ApiController.php
+++ api/common/controllers/ApiController.php
@@ -2,10 +2,19 @@
 
 namespace api\common\controllers;
 
+use Yii;
 use yii\rest\Controller;
 
 /**
  * Api controller.
+ */
+
+/**
+ * @OA\Info(
+ *   version="1.0",
+ *   title="ATuPuerta API",
+ *   description="Server - ATuPuerta app API",
+ * ),
  */
 class ApiController extends Controller
 {
@@ -14,11 +23,40 @@ class ApiController extends Controller
      *
      * @return mixed
      */
+
+    /**
+     * @OA\Get(path="/",
+     *   summary="API Info",
+     *   tags={"default"},
+     *   @OA\Response(
+     *     response=200,
+     *     description="Return API info",
+     *   ),
+     * )
+     */
     public function actionIndex()
     {
         return $this->asJson([
             'message' => 'ATuPuerta API',
             'last_version' => 'v1',
         ]);
+    }
+
+    public function actions()
+    {
+        return [
+            'docs' => [
+                'class' => 'genxoft\swagger\ViewAction',
+                'apiJsonUrl' => \yii\helpers\Url::to(['/api/api-json'], true),
+            ],
+            'api-json' => [
+                'class' => 'genxoft\swagger\JsonAction',
+                'dirs' => [
+                    Yii::getAlias('@api/common/controllers'),
+                    Yii::getAlias('@api/modules/v1/controllers'),
+                    Yii::getAlias('@api/modules/v1/models'),
+                ],
+            ],
+        ];
     }
 }
